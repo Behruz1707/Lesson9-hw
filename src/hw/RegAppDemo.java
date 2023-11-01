e268cd22488bef5a64ced2ca2545f5a79fa62b5b
Behruzbek <behruznosirjonov23@gmail.com>
2023-11-01 11:54:43 +0300

    commit from beginning of project

---
+++ src/hw/RegAppDemo.java
@@ -0,0 +1,11 @@
+package hw;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RegAppDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            
+    }
+}
---
+++ src/hw/UserInfo.java
@@ -0,0 +1,47 @@
+package hw;
+
+public class UserInfo {
+    private String fullName;
+    private String gmail;
+    private String phoneNumber;
+    private String password;
+
+    public UserInfo(String fullName, String gmail, String phoneNumber, String password) {
+        this.fullName = fullName;
+        this.gmail = gmail;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
